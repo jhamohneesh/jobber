3b0b86560d330df79f7e8f5cfd443db422eb0b3c
jhamohneesh <78133085+jhamohneesh@users.noreply.github.com>
2021-02-04 17:37:38 +0530

    Update arm64_test.sh

--- .github/scripts/arm64_test.sh
+++ .github/scripts/arm64_test.sh
@@ -10,4 +10,4 @@ su jobber -c "abuild-keygen -an"
 find "/home/jobber/.abuild" -name '*.rsa.pub' -exec mv {} /etc/apk/keys/ ';'
 chown root:root /etc/apk/keys/*
 chmod a+w . packaging/alpine
-su jobber -c "make -C packaging/alpine pkg-local DESTDIR=/root/runner/pkgs/"
+su jobber -c "echo "$PWD" && make -C packaging/alpine pkg-local DESTDIR=$PWD/pkgs/"
